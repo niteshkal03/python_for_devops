fb38c2b69620175466c2dc4fa362edcef4967241
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-04 17:56:41 +0530

    new file added

---
+++ 04_may_string.py
@@ -0,0 +1,39 @@
+student_name = "Nitesh Kumar" #single inverted quot we cant use again and again like "wdwq wdwd "dwqdwq" dwqq"
+print(f"{student_name}")
+
+print(f'''Hii Everyone, My Name is Nitesh {student_name} and
+I am from Ghaziabad. I have Completed My "Graduation"
+from BScIT''') #we need to use single inverted quotes to use highlight word like graduation
+
+#****Operator*****
+# 1.Arithmatic Operator
+# 2.Assignment Operator
+# 3.Comparision/Relational Operator
+# 4.Logical Operator
+# 5.Membership Operator
+# 6.Indentity Operator
+# 7.Bitwise Operator
+
+# 2.Assignment Operator 
+# Assignment Operator is nothing but assign a value in a Variable
+a=10
+a=a+10 #standard method
+a+=10 #shorthand method
+
+# 3.Comparision/Relational Operator(>,<,>=,<=,==,!=)
+n1 = 30
+n2 = 40
+res1 = n1>n2
+res2 = n1<n2
+print(res1)
+print(res2)
+
+str1="A"
+str2="aman kumar"
+print(str1>str2)
+
+# 4.Logical Operator(and, or, not)
+n1=1
+n2=3
+res=n1==1 and n2==3
+print(res) #true
