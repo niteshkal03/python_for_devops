62bbf34bd0551709083f5d50d45033eeb90c6eb5
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-05 10:45:49 +0530

    5th may practice work added

---
+++ 05_may.py
@@ -0,0 +1,34 @@
+# Membership Operator
+# its check sequence if its match the will true
+#its not work on numbers, only work on strings
+#this operator work on (not, in)
+# str1 = "this is python for devops"
+# find = "this"
+# print("this" in str1)
+
+# num1 = "3434" #no working on integer value
+# print("4" in num1)
+
+# find = "3" and "6" # its jump on last value 
+# print(find in num1)
+
+email="iqindia123@gmail.com"
+find = "@gmail.com"
+if find in email:
+# if "@gmail.com" in email:
+    print("Valid")
+else:
+    print("Invalid")
+
+
+min_age = 18
+nationality = "Indian"
+user_age = int(input("Enter Your Age: "))
+user_id = input("Enter Your Nationality: ")
+
+if user_age >= min_age and user_id == nationality:
+    print("Your Are Eligible for Voting")
+
+else:
+    print("Your Are Not Eligible for Voting")
+    
