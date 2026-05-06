4ca06c1ea7a76ca3fe51edeaba5e525db8bc7023
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-06 06:58:47 +0530

    Practice Question Added

---
+++ 05_if_else.py
@@ -0,0 +1,9 @@
+# check if a single character is a vowel or not
+vowels = "a,e,i,o,u"
+# input_letter = input("Enter Your Letter: ")
+find = "a,e,i"
+
+if find in vowels:
+    print("Yes, Its Vowels!")
+else:
+    print("Its Not Vowels")
