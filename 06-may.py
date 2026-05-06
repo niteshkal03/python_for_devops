ed35886768f63238be7e0d517b4b2988fbad56f9
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-06 22:22:55 +0530

    new practice work file added

---
+++ 06-may.py
@@ -0,0 +1,36 @@
+# 6. Indentity Operator: (is, is not)
+#compare address of number
+# most of tyhe cases use in number
+# a = 1000
+# b = 1000
+
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+
+# even number = which divide by 2 and remainder 0
+
+#wap to print the last digit of number is 456735
+# val = "32434"
+# print(f"last number is: {val[-1]}")
+
+# print(433454 % 10) #last 1 number print
+# print(433454 % 100) #last 2 number print
+
+#wap 
+num1 = 3
+num2 = 9
+num3 = 1
+
+#wap to find largest
+
+#wap to find smallest
+if num1<=num2 and num1<=num3:
+    print(f"num1 is smallest")
+
+if num2<=num1 and num2<=num3:
+    print(f"num2 is smallest")
+
+if num3<=num1 and num3<=num2:
+    print(f"num3 is smallest")
