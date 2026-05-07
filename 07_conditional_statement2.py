df2541df36e2af7c2811a1e2396e9a89c7dcf980
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-07 16:17:52 +0530

    Improvement in Practice Code

--- 07_conditional_statement2.py
+++ 07_conditional_statement2.py
@@ -1,6 +1,16 @@
-letters = input("Enter Your Letter : ")
+# WAP to check if a single character is a vowel or not.
 vowels ="aeiou"
-if letters in vowels:
-    print(f"Yes, {letters} is vowels")
+
+letters = input("Enter Your Letter : ")
+
+#len() is used for count the number of letters or space and lstrip() use for remove space count
+letter_len = len(letters.lstrip())
+
+if letters:
+    if letter_len == 1:
+        if letters.lower().lstrip() in vowels:           #Lower(): Uppercase letter converts into lower case
+            print(f"Yes, {letters} is vowels")
+        else:
+            print(f"{letters} is Not vowels")
 else:
-    print(f"{letters} is Not vowels")
+    print("Please Enter Letter first")
