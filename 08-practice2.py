91a7d3e47e9acf680bfeca1ad1299af5f9eb6b5e
niteshkal03 <niteshkalangada888@gmail.com>
2026-05-08 16:54:15 +0530

    Changes in Practice Questions

--- 08-practice2.py
+++ 08-practice2.py
@@ -5,7 +5,6 @@
 # ....
 # 3x10=30
 usr_input = int(input("Enter Your Number: "))
-s = 0
 for i in range (1,11):
-    s = usr_input * i
-    print(f"{usr_input} x {i} = {s}")
+    ans = usr_input * i
+    print(f"{usr_input} x {i} = {ans}")
